2c23dc1805d0cf95bbdfc81dedb9edda2d323b56
thinh le <tql180000@utdallas.edu>
2022-01-16 20:57:36 -0600

    added message box

--- src/main/java/com/thinhle/helloworld/App.java
+++ src/main/java/com/thinhle/helloworld/App.java
@@ -1,5 +1,7 @@
 package com.thinhle.helloworld;
 
+import javax.swing.JOptionPane;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        JOptionPane.showMessageDialog(null, "hello world!", "example", JOptionPane.INFORMATION_MESSAGE);
     }
 }
