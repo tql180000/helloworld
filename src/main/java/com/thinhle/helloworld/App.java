1294aba1118e2b7f487e910e5ad6edc6fd52fd8a
thinh le <tql180000@utdallas.edu>
2022-01-16 21:32:41 -0600

    added basic addition

--- src/main/java/com/thinhle/helloworld/App.java
+++ src/main/java/com/thinhle/helloworld/App.java
@@ -6,10 +6,19 @@ import javax.swing.JOptionPane;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "hello world!", "example", JOptionPane.INFORMATION_MESSAGE);
+
+        int a, b;
+
+        a = Integer.parseInt(JOptionPane.showInputDialog(null, "enter an integer: "));
+        b = Integer.parseInt(JOptionPane.showInputDialog(null, "enter an integer: "));
+
+        JOptionPane.showMessageDialog(null, "sum = " + add(a, b), "example", JOptionPane.INFORMATION_MESSAGE);
     }
 }
